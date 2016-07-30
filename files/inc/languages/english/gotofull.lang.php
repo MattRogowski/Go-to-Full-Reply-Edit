16299d2f081dc9b99ca2dd93f9c40e879968a9cd
Matt Rogowski <mattrogowski@Matt-Rogowskis-Mackbook-Pro.local>
2016-07-30 20:07:14 +0100

    1.8 compatibility

--- files/inc/languages/english/gotofull.lang.php
+++ files/inc/languages/english/gotofull.lang.php
@@ -1,9 +1,20 @@
 <?php
 /**
- * Go to Full Reply/Edit 0.1.1 - Language File
- * © MattRogowski 2010
- * http://mattrogowski.co.uk
- * You may edit for your own personal forum but must not redistribute it in any form without my permission.
+ * Go to Full Reply/Edit 1.1 - Language File
+
+ * Copyright 2016 Matthew Rogowski
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ ** http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
 **/
 
 $l['gotofull_reply'] = "Go to Full Reply";
--- files/inc/plugins/gotofull.php
+++ files/inc/plugins/gotofull.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Go to Full Reply/Edit 0.1.1
+ * Go to Full Reply/Edit 1.1
 
- * Copyright 2010 Matthew Rogowski
+ * Copyright 2016 Matthew Rogowski
 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,18 +25,17 @@ if(!defined("IN_MYBB"))
 $plugins->add_hook("global_start", "gotofull_reply");
 $plugins->add_hook("showthread_start", "gotofull_reply_button");
 $plugins->add_hook("editpost_end", "gotofull_edit");
-$plugins->add_hook("xmlhttp", "gotofull_edit_button");
 
 function gotofull_info()
 {
 	return array(
 		"name" => "Go to Full Reply/Edit",
 		"description" => "Allows you to quickly go from quick reply to full reply or quick edit to full edit whilst saving any text you had previously entered.",
-		"website" => "http://mattrogowski.co.uk",
-		"author" => "MattRogowski",
-		"authorsite" => "http://mattrogowski.co.uk",
-		"version" => "0.1.1",
-		"compatibility" => "16*",
+		"website" => "https://github.com/MattRogowski/Go-to-Full-Reply-Edit",
+		"author" => "Matt Rogowski",
+		"authorsite" => "https://matt.rogow.ski",
+		"version" => "1.1",
+		"compatibility" => "16*,18*",
 		"guid" => "43a080234dcf1acbb6e38dc0dd843279"
 	);
 }
@@ -54,26 +53,19 @@ function gotofull_activate()
 		"title" => "gotofull_reply",
 		"template" => "<input type=\"submit\" class=\"button\" name=\"gotofullreply\" value=\"{\$lang->gotofull_reply}\" tabindex=\"3\" />"
 	);
-	$templates[] = array(
-		"title" => "gotofull_edit",
-		"template" => "<input type=\"hidden\" name=\"pid\" value=\"{\$post['pid']}\" />
-<input type=\"hidden\" name=\"message\" id=\"gotoquickedit_message\" value=\"\" />
-<input type=\"submit\" name=\"gotofulledit\" value=\"{\$lang->gotofull_edit}\" onclick=\"form = \$('pid_{\$post['pid']}').childElements()[0];form.action = 'editpost.php';form.method = 'post';\$('gotoquickedit_message').value = \$('quickedit_{\$post['pid']}').value;\" />"
-	);
 	foreach($templates as $template)
 	{
 		$insert = array(
 			"title" => $db->escape_string($template['title']),
 			"template" => $db->escape_string($template['template']),
 			"sid" => "-1",
-			"version" => "1600",
+			"version" => "1800",
 			"dateline" => TIME_NOW
 		);
 		$db->insert_query("templates", $insert);
 	}
 	
 	find_replace_templatesets("showthread_quickreply", "#".preg_quote('<input type="submit" class="button" name="previewpost" value="{$lang->preview_post}" tabindex="3" />')."#i", '<input type="submit" class="button" name="previewpost" value="{$lang->preview_post}" tabindex="3" />{$gotofull_reply}');
-	find_replace_templatesets("xmlhttp_inline_post_editor", "#".preg_quote('<input type="button" class="button" onclick="Thread.quickEditCancel({$post[\'pid\']});" value="{$lang->cancel_edit}" tabindex="1001" />')."#i", '<input type="button" class="button" onclick="Thread.quickEditCancel({$post[\'pid\']});" value="{$lang->cancel_edit}" tabindex="1001" />{$gotofull_edit}');
 }
 
 function gotofull_deactivate()
@@ -83,14 +75,12 @@ function gotofull_deactivate()
 	require_once MYBB_ROOT . "inc/adminfunctions_templates.php";
 	
 	$templates = array(
-		"gotofull_reply",
-		"gotofull_edit"
+		"gotofull_reply"
 	);
 	$templates = "'" . implode("','", $templates) . "'";
 	$db->delete_query("templates", "title IN ({$templates})");
 	
 	find_replace_templatesets("showthread_quickreply", "#".preg_quote('{$gotofull_reply}')."#i", '', 0);
-	find_replace_templatesets("xmlhttp_inline_post_editor", "#".preg_quote('{$gotofull_edit}')."#i", '', 0);
 }
 
 function gotofull_reply_button()
@@ -100,6 +90,30 @@ function gotofull_reply_button()
 	$lang->load("gotofull");
 	
 	eval("\$gotofull_reply = \"".$templates->get('gotofull_reply')."\";");
+
+	$script = "<script>
+	jQuery(document).ready(function() {
+		jQuery('body').on('click', '.postbit_edit', function() {
+			quick_edit_id = jQuery(this).next('.popup_menu').find('.quick_edit_button').attr('id');
+			post_id = quick_edit_id.match(/\d+$/)[0];
+			jQuery('#'+quick_edit_id).click(function() {
+				that = this;
+				setTimeout(function() {
+					form = jQuery(that).parents('.post').find('form');
+					if(!jQuery(form).find('.gotofulledit').length)
+					{
+						button = jQuery('<button/>', {'type':'submit','text':'".$lang->gotofull_reply."','name':'gotofulledit','class':'gotofulledit','value':'1'});
+						jQuery(form).find('button:last').after(button);
+						button.on('click', function() {
+							jQuery(form).attr('method','post').attr('action','editpost.php?pid='+post_id).unbind('submit').find('.gotofulledit').click();
+						});
+					}
+				}, 10);
+			});
+		});
+	});
+	</script>";
+	eval("\$gotofull_reply .= \"".$script."\";");
 }
 
 function gotofull_reply()
@@ -112,27 +126,13 @@ function gotofull_reply()
 	}
 }
 
-function gotofull_edit_button()
-{
-	global $mybb, $lang, $templates, $gotofull_edit;
-	
-	if($mybb->input['action'] == "edit_post")
-	{
-		$lang->load("gotofull");
-		
-		$post = get_post($mybb->input['pid']);
-		
-		eval("\$gotofull_edit = \"".$templates->get('gotofull_edit')."\";");
-	}
-}
-
 function gotofull_edit()
 {
 	global $mybb, $message;
 	
 	if($mybb->input['gotofulledit'])
 	{
-		$message = $mybb->input['message'];
+		$message = $mybb->input['value'];
 	}
 }
 ?>
